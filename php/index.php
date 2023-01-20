7dd376239fbaa69303dea7f1bddc7fe95ec608e7
aqeelaman <122634304+aqeelaman@users.noreply.github.com>
2023-01-20 20:39:34 +0400

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,21 @@
+<?php
+    //Include the PHP functions to be used on the page 
+    include('common.php'); 
+	
+    //Output header and navigation 
+    outputHeader("HOME");
+    outputBannerNavigation("HOME");
+?>
+
+<!-- Contents of the page -->
+<h1 class="heading">FRESH VEGGIES AT YOUR DOORSTEP</h1>
+        
+<!--div for the home page contents-->
+<div class="featured_container">
+    <img src="https://naturescart.ae/content/images/products/SMGT11491V0120220607124903.jpg"  alt="potato" >
+</div>
+
+<?php
+    //Output the footer
+    outputFooter();
+?>
