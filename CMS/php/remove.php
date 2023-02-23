c3eae43e85f1b015c601a9adf9f120cbb35cb69c
https-hd <123236149+https-hd@users.noreply.github.com>
2023-02-23 19:57:52 +0400

    changes

--- CMS/php/remove.php
+++ CMS/php/remove.php
@@ -1,36 +1,25 @@
 <?php
 
-//Include libraries
 require __DIR__ . '/vendor/autoload.php';
 
-//Create instance of MongoDB client
-    $mongoClient = (new MongoDB\Client);
+$mongoClient = new MongoDB\Client;
 
-    $db = $mongoClient->stuffington;
-    $collection = $db->Order;
+$db = $mongoClient->stuffington;
+$collection = $db->Order;
 
-    $OrderID = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
+$OrderID = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
 
+$DeleteCriteria = [
+    "OrderID" => $OrderID
+];
 
-    $DeleteCriteria = [
-        "OrderID" => $OrderID
-    ];
-    
-    echo $OrderID;
+$returnVal = $collection->deleteOne($DeleteCriteria);
 
-    $returnVal = $collection->deleteOne($DeleteCriteria);
+if ($returnVal->getDeletedCount() == 1) {
+    header("Location: ../HTML/order.html");
+} else {
 
-
-    if ($returnVal->getDeletedCount() == 1) {
-        header("Location: ../HTML/order.html");
-        }
-
-     else {
-          echo "Order not deleted";
-    }
-
-
-
-
+    echo "Order not deleted";
+}
 
 ?>
