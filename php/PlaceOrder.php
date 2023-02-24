b95226fb308f04d67e6b759739611dc475a4d4e6
aqeelaman <AA5226@live.mdx.ac.uk>
2023-02-24 19:28:03 +0400

    push

--- php/PlaceOrder.php
+++ php/PlaceOrder.php
@@ -9,18 +9,45 @@ $mongoClient = (new MongoDB\Client);
 $db = $mongoClient->stuffington;
 
 //select a collection
-$collection = $db->orders;
+$collection = $db->order;
 
 //extract from session storage
 //$user = "<script language='JavaScript'>var n=sessionStorage.getItem('user');document.write(n);</script>";
 
-//$customer_idStr = filter_input(INPUT_POST, 'customer_id', FILTER_SANITIZE_STRING);
-//$shipping_address = filter_input(INPUT_POST, 'shipping_array',FILTER_SANITIZE_STRING);
-//$totalStr = filter_input(INPUT_POST,"price",FILTER_SANITIZE_STRING);
-$productList = file_get_contents( "php://input" );
+$customer_idStr = filter_input(INPUT_POST, 'customer_id', FILTER_SANITIZE_STRING);
+$shipping_address = filter_input(INPUT_POST, 'shipping_address',FILTER_SANITIZE_STRING);
+$totalStr = filter_input(INPUT_POST,"total",FILTER_SANITIZE_STRING);
+floatval($totalStr);
+$prodIdStr = filter_input(INPUT_POST, 'prodId', FILTER_SANITIZE_STRING);
+$qtyStr=filter_input(INPUT_POST, 'qty', FILTER_SANITIZE_STRING);
+intval($qtyStr);
+//$productList = file_get_contents( "php://input" );
 //$productList = json_decode($productList);
 //filter_input(INPUT_POST,"cartList",FILTER_SANITIZE_STRIPPED);
 
 //$price = $productList["price"];
-echo $productList;
+
+
+date_default_timezone_set("Asia/Dubai");
+$order = [
+    "customer_id" => new MongoDB\BSON\ObjectID($customer_idStr),
+    "shipping_address" => $shipping_address,
+    "date" => date("d/m/Y"),
+    "time" => date("H:i"),
+    "price" => $totalStr,
+    "products" => array(
+        "product_id" => new MongoDB\BSON\ObjectID($prodIdStr),
+        "quantity" => $qtyStr
+    )
+    ];
+
+$result = $collection->insertOne($order);
+
+//Check if the insertion was successful
+if ($result->getInsertedCount() == 1) {
+    echo "Order added";
+} else {
+    echo "Order not added";
+}
+
 ?>
