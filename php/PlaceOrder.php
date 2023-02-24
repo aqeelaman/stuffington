7656b703a450af1b3cef714f22a1d36fadb2be71
aqeelaman <AA5226@live.mdx.ac.uk>
2023-02-24 18:36:18 +0400

    checkout?

--- php/PlaceOrder.php
+++ php/PlaceOrder.php
@@ -12,6 +12,15 @@ $db = $mongoClient->stuffington;
 $collection = $db->orders;
 
 //extract from session storage
-$user = "<script language='JavaScript'>var n=sessionStorage.getItem('user');document.write(n);</script>";
+//$user = "<script language='JavaScript'>var n=sessionStorage.getItem('user');document.write(n);</script>";
 
+//$customer_idStr = filter_input(INPUT_POST, 'customer_id', FILTER_SANITIZE_STRING);
+//$shipping_address = filter_input(INPUT_POST, 'shipping_array',FILTER_SANITIZE_STRING);
+//$totalStr = filter_input(INPUT_POST,"price",FILTER_SANITIZE_STRING);
+$productList = file_get_contents( "php://input" );
+//$productList = json_decode($productList);
+//filter_input(INPUT_POST,"cartList",FILTER_SANITIZE_STRIPPED);
+
+//$price = $productList["price"];
+echo $productList;
 ?>
